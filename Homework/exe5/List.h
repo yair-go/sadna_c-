4d70d2c521f75ed76596a4be6143e9eaf624e247
Yair Goldshtein <irigo1@gmail.com>
2019-09-05 19:27:32 +0300

    Create List.h

---
+++ Homework/exe5/List.h
@@ -0,0 +1,41 @@
+ 	//------------------------------------------------
+ 	//  class List
+ 	//      arbitrary size Lists
+ 	//      permits insertion and removal 
+ 	//      only from the front of the List
+ 	//------------------------------------------------
+ 	class List
+ 	{
+ 	protected:
+	  	//--------------------------------------------
+		  	//  inner class link
+		  	//  a single element for the linked List 
+		  	//--------------------------------------------
+		  	class Link
+		  	{
+		  	public:
+			  	// constructor
+				  	Link(int linkValue, Link * nextPtr);
+			  		Link(const Link &);
+			  	// data areas
+				  	int value;
+			  		Link * next;
+			  	};	//end of class Link
+  	public:
+	  	// constructors
+		List();
+	  	List(const List&);
+	  	~List();
+
+	  	// operations
+		void add(int value);
+	  	int firstElement() const;
+	  	bool search(const int &value) const;
+	  	bool isEmpty() const;
+	  	void removeFirst();
+	  	void clear();
+
+  	protected:
+	  	// data field
+		  	Link* head;
+	  	};
